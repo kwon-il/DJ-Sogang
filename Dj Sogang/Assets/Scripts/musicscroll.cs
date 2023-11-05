606c9b70b61de554314d9c9893847e3bf21765eb
kwon-il <81847281+kwon-il@users.noreply.github.com>
2023-11-05 16:55:02 +0900

    main 화면 구현완료
    초기 프리팹 삭제해야함.
    게임화면 scene으로 변경 구현 해야함

---
+++ Dj Sogang/Assets/Scripts/musicscroll.cs
@@ -0,0 +1,164 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class musicscroll : MonoBehaviour
+{
+    public GameObject itemPrefab; // Inspector에서 프리팹 할당
+    public Transform contentPanel; // Inspector에서 Content GameObject 할당
+    public float itemSpacing = 20f; // 아이템 간의 간격을 설정합니다.
+    public ScrollRect scrollRect; // Inspector에서 ScrollRect를 할당합니다.
+    private List<GameObject> items = new List<GameObject>(); // 아이템들의 리스트
+    private int selectedIndex = 0; // 현재 선택된 아이템의 인덱스
+    public Image mainSceneImage; // Inspector에서 메인 씬의 Image UI 요소 할당
+
+    void Start()
+    {
+        LoadMusic();
+        if (items.Count > 0)
+        {
+            HighlightItem(); // 시작 시 첫 번째 아이템을 강조합니다.
+        }
+    }
+
+    void Update()
+{
+    // 사용자의 키보드 입력을 확인합니다.
+    if (Input.GetKeyDown(KeyCode.UpArrow))
+    {
+        // 맨 위에서 위쪽 화살표를 누른 경우, 맨 아래로 이동합니다.
+        if (selectedIndex <= 0)
+        {
+            selectedIndex = items.Count - 1;
+        }
+        else
+        {
+            selectedIndex--;
+        }
+
+        HighlightItem();
+        ScrollToSelected();
+    }
+    else if (Input.GetKeyDown(KeyCode.DownArrow))
+    {
+        // 맨 아래에서 아래쪽 화살표를 누른 경우, 맨 위로 이동합니다.
+        if (selectedIndex >= items.Count - 1)
+        {
+            selectedIndex = 0;
+        }
+        else
+        {
+            selectedIndex++;
+        }
+
+        HighlightItem();
+        ScrollToSelected();
+    }
+}
+
+    void LoadMusic()
+    {
+        AudioClip[] musicTracks = Resources.LoadAll<AudioClip>("Music");
+        float contentHeight = musicTracks.Length * itemSpacing;
+        ((RectTransform)contentPanel).sizeDelta = new Vector2(((RectTransform)contentPanel).sizeDelta.x, contentHeight);
+
+        for (int i = 0; i < musicTracks.Length; i++)
+        {
+            AudioClip track = musicTracks[i];
+            Sprite photo = Resources.Load<Sprite>("Photo/" + track.name);
+            TextAsset textAsset = Resources.Load<TextAsset>("Text/" + track.name);
+
+            GameObject newItem = Instantiate(itemPrefab, contentPanel);
+            items.Add(newItem);
+            Image imageComponent = newItem.GetComponentInChildren<Image>();
+
+            // Image 컴포넌트에 사진을 할당합니다.
+            if (imageComponent != null && photo != null)
+            {
+                imageComponent.sprite = photo;
+            }
+            else
+            {
+                Debug.LogError("Image 컴포넌트 또는 사진이 로드되지 않았습니다: " + track.name);
+            }
+
+            
+            GameObject textPrefab = Resources.Load<GameObject>("Text/" + track.name);
+            if (textPrefab != null)
+            {
+                // 로드된 텍스트 프리팹에서 Text 컴포넌트를 찾습니다.
+                Text textComponent = textPrefab.GetComponentInChildren<Text>();
+                if (textComponent != null)
+                {
+                    // 현재 아이템의 Text 컴포넌트를 찾아 텍스트를 설정합니다.
+                    Text itemTextComponent = newItem.GetComponentInChildren<Text>();
+                    if (itemTextComponent != null)
+                    {
+                        itemTextComponent.text = textComponent.text;
+                    }
+                }
+                else
+                {
+                    Debug.LogError("프리팹 내의 Text 컴포넌트를 찾을 수 없습니다: " + track.name);
+                }
+            }
+            else
+            {
+                Debug.LogError("텍스트 프리팹을 로드할 수 없습니다: " + track.name);
+            }
+        }
+        
+    }
+    void HighlightItem()
+    {
+    // 모든 아이템을 반복합니다.
+    for (int i = 0; i < items.Count; i++)
+    {   
+        if (i == selectedIndex)
+        {
+            // 메인 씬의 이미지를 업데이트합니다.
+            if (mainSceneImage != null && items[selectedIndex].GetComponentInChildren<Image>().sprite != null)
+            {
+                mainSceneImage.sprite = items[selectedIndex].GetComponentInChildren<Image>().sprite;
+            }
+        }
+        // 각 아이템에서 'Background'라는 이름의 패널을 찾습니다.
+        Transform backgroundPanel = items[i].transform.Find("Background");
+
+        if (backgroundPanel != null)
+        {
+            Image backgroundImage = backgroundPanel.GetComponent<Image>();
+            if (backgroundImage != null)
+            {
+                // 선택된 아이템이면 파란색으로, 아니면 기본 색상으로 설정합니다.
+                backgroundImage.color = (i == selectedIndex) ? Color.green : Color.grey;
+            }
+            else
+            {
+                Debug.LogError("Background 패널에 Image 컴포넌트가 없습니다: " + items[i].name);
+            }
+        }
+        else
+        {
+            Debug.LogError("아이템 내에 'Background'라는 이름의 자식이 없습니다: " + items[i].name);
+        }
+        
+    }
+    }
+
+    void ScrollToSelected()
+{
+    // scrollRect의 null 체크를 추가합니다.
+    if (scrollRect != null)
+    {
+        float normalizePosition = (float)selectedIndex / (items.Count - 1);
+        scrollRect.verticalNormalizedPosition = 1 - normalizePosition;
+    }
+    else
+    {
+        Debug.LogError("ScrollRect가 할당되지 않았습니다.");
+    }
+}
+}
